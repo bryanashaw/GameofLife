1e77cce00f08a9a156c64dbd32b439fddfb9dc56
Bryan Shaw <blurchill@gmail.com>
2022-02-19 17:10:43 -0500

    Cell Class
    Fleshing out cell class. Thinking of using an event to handle population handling instead of looping through a 2d array.
    
    The board would call the even and it would fire on all of the cells who have subscribed.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -7,7 +8,9 @@ public class Board : MonoBehaviour
     [SerializeField] private int gridWidth = 100;
     [SerializeField] private Cell cellPrefab;
 
-    private Cell[,] _grid;
+    private Cell[,] _boardofCells;
+    
+    public event Action onNextGeneration;
 
 
     // Start is called before the first frame update
@@ -18,15 +21,33 @@ public class Board : MonoBehaviour
 
     private void PlaceCells()
     {
-        _grid = new Cell[gridHeight, gridWidth];
+        _boardofCells = new Cell[gridHeight, gridWidth];
         for (int z = 0; z < gridHeight; z++)
         {
             for (int x = 0; x < gridWidth; x++)
             {
-               var cell = Instantiate(cellPrefab, new Vector3(x, 0, z), quaternion.identity);
-               cell.transform.SetParent(gameObject.transform);
-               _grid[x, z] = cell;
+                var cell = Instantiate(cellPrefab, new Vector3(x, 0, z), quaternion.identity);
+                cell.transform.SetParent(gameObject.transform);
+                _boardofCells[x, z] = cell;
             }
+        }
+    }
+
+    private void CheckNeighbors()
+    {
+        for (int z = 0; z < gridHeight; z++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                var numofNeighbors = 0;
+
+                if (z + 1 < gridHeight)
+                {
+
+                }
+
+            }
+            
         }
     }
 
--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Material aliveMaterial;
+    [SerializeField] private Material deadMaterial;
+
+    private bool isAlive;
+    private int numofNeighbors;
+
+
+    private void OnEnable()
     {
-        
+        GetComponent<Board>().onNextGeneration += PopulationControl;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        GetComponent<Board>().onNextGeneration -= PopulationControl;
+    }
+
+    private void SetNeighbors(int num)
+    {
+        numofNeighbors = num;
+    }
+
+    // private void SetLifeStatus(bool status)
+    // {
+    //     isAlive = status;
+    // }
+
+    private void PopulationControl()
+    {
+        if (isAlive)
+        {
+            if (numofNeighbors != 2 && numofNeighbors != 3)
+            {
+                isAlive = false;
+            }
+        }
+        else
+        {
+            if (numofNeighbors == 3)
+            {
+                isAlive = true;
+            }
+        }
     }
 }
